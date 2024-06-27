d9c03f5ea53fb1ded073849081bc1328f0e0ee11
taLita Santos <100318502+euLita@users.noreply.github.com>
2024-06-27 13:35:49 +0000

    update

--- toDolist/todos/views.py
+++ toDolist/todos/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def home(request):
-    return render(request, "todos/home.html")
-    # return HttpResponse("Hello, Treining!")
-
 
 # Create your views here.
+def todo_list(request):
+    return render(request."todos/todo_list.html")
