4a6c7242e309f91cf89e7670dea4d2ed13c4c565
taLita Santos <100318502+euLita@users.noreply.github.com>
2024-07-12 21:04:46 +0000

    estou tendo dificuldade em sair desta parte

--- toDolist/setup/urls.py
+++ toDolist/setup/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from todos.views import todo_list
+
 urlpatterns = [
-    path("admin/", admin.site.urls)
+    path("admin/", admin.site.urls), 
+    path("", todo_list),
 ]
--- toDolist/todos/views.py
+++ toDolist/todos/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def todo_list(request):
-    return render(request."todos/todo_list.html")
+    return render(request,"todos/todo_list.html")
